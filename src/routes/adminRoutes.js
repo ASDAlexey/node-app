1e3286c5deb8a238f1fc8de83890fe0d6cc4aa44
ASDAlexey <asdalexey@yandex.ru>
2016-08-14 22:12:59 +0300

    add sequelize node.js db  orm

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,38 +1,79 @@
 const express = require('express');
 const adminRouter = express.Router();
-const mongodb = require('mongodb').MongoClient;
+const mysql = require('mysql');
+const Sequelize = require('sequelize');
+// connection string
+const sequelize = new Sequelize('node-app', 'root', '121314', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000,
+    },
+});
+
+// model schema
+const Books = sequelize.define('books', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    title: {
+        type: Sequelize.STRING,
+        unique: true,
+        defaultValue: 'Default title',
+        validate: {
+            len: {
+                args: [3, 150],
+                msg: 'Lenght must be morwe then 10 and less then 150 symbols',
+            },
+        },
+    },
+    genre: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    author: {
+        type: Sequelize.STRING,
+        validate: {
+            startWithUpper(authorVal){
+                const first = authorVal.charAt(0);
+                const startWithUpper = first === authorVal[0].toUpperCase();
+                if (!startWithUpper) {
+                    throw new Error('First letter must be in uppercase');
+                }
+            },
+        },
+    },
+    read: Sequelize.BOOLEAN,
+});
 
 const router = (nav) => {
-    const books = [
-        {
-            title: 'AAAA1',
-            genre: 'SSSS',
-            author: 'ASDA',
-            read: false,
-        },
-        {
-            title: 'AAAA2',
-            author: 'ASDA',
-            read: false,
-        },
-        {
-            title: 'AAAA3',
-            genre: 'SSSS',
-            author: 'ASDA',
-            read: false,
-        },
-    ];
     adminRouter.get('/addBooks', (req, res) => {
-        const url = 'mongodb://localhost:27017/libraryApp';
-        mongodb.connect(url, (error, db) => {
-            const collection = db.collection('books');
-            collection.insertMany(books, (err, results) => {
-                res.send(results);
+        // create DB table if one does not exist already
+        sequelize.sync({
+            logging: console.log,
+        }).then(() => {
+            // add books to table
+            Books.create({
+                title: 'asdalexey',
+                genre: '',
+                author: 'Aasd',
+                read: false,
+            }).then((data) => {
+                // Books.findById(1).then((foundedObj) => {
+                //     // console.log(foundedObj.dataValues);
+                //
+                // });
+                res.send(data.dataValues);
+            }).catch((err) => {
+                res.send(err);
             });
-            db.close();
+
         });
     });
-
     return adminRouter;
 };
 
